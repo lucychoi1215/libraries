5f3be03bcb20c83724a450fdda4e307ff2ed6296
unknown <jsc717@nyu.edu>
2020-04-19 21:31:48 +0400

    redraft

--- script.js
+++ script.js
@@ -34,23 +34,9 @@ window.addEventListener('mousemove', function(event) {
 })(jQuery);
 
 ///
+
 $('.grid').masonry({
   // options
   itemSelector: '.grid-item',
   columnWidth: 200
 });
-
-///
-
-// // prefixed
-//
-// const instance = Bricks({
-//   packed: 'data-packed'
-// })
-//
-// // unprefixed
-//
-// const instance = Bricks({
-//   // becomes: 'data-packed'
-//   packed: 'packed'
-// })
